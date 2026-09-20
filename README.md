314408027a2b73825ef542d76caeb60f7364d7ad
Mitchell <mdj43@students.uwf.edu>
2026-09-20 17:23:15 -0400

    Add skeleton for login.php and update README.md

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Colors is a simple web app built using a LAMP stack. Users can log in through th
 2. Install composer at https://getcomposer.org in /var/www/html/api.
 3. Create the MySQL database on the web server using database.sql.
 4. Create a database user for the API so it can interact with the database.
-5. Create a .env file in /var/www/html/api with the database user credentials.
+5. Create a .env file in /var/www/html/api with the database user credentials (DB_HOST, DB_USER, DB_PASSWORD, and DB_NAME).
 6. Insert at least one user account into the database.
 ## Access Instructions
 - Access your colors application at http://<your Apache server's IP address>.
---
+++ api/login.php
@@ -0,0 +1,39 @@
+<?php
+    /*
+     * The following lines starting with "require_once" through "$connection"
+     * should appear at the top of every PHP file. This ensures the .env is 
+     * being used and that a connection to the database is established.
+     */
+    
+    require_once __DIR__ . '/vendor/autoload.php';
+    $dotEnv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotEnv->load();
+
+    $inData = json_decode(file_get_contents('php://input'), true);
+    $connection = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'],
+                             $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+
+    if ($connection->connect_error)
+    {
+
+    }
+    else
+    {
+
+    }
+
+    function returnWithError($error)
+    {
+
+    }
+
+    function returnSuccess($id)
+    {
+
+    }
+
+    function sendResultInfoAsJson($object)
+    {
+        
+    }
+?>
