a444b393a792a500ce24d75c1d27c71b23e53044
Mitchell <mdj43@students.uwf.edu>
2026-09-26 12:43:06 -0400

    Adds code.js code skeleton

---
+++ public/js/code.js
@@ -0,0 +1,36 @@
+const urlBase = 'http://collateralconscience.store/API';
+const extension = 'php';
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+function doLogin()
+{
+
+}
+
+function saveCookie()
+{
+
+}
+
+function readCookie()
+{
+
+}
+
+function doLogout()
+{
+
+}
+
+function addColor()
+{
+
+}
+
+function searchColor()
+{
+
+}
